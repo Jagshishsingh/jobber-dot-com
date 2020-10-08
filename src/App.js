bfda694b7d05f19147c16e6eb12115f753cf7651
Jagshish Singh <singhjagshish0001@gmail.com>
2020-10-08 22:22:22 +0530

    home page: navbar and body login buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Body from './Components/Body';
+import HomeNavbar from './Components/HomeNavbar';
+import LoginBtns from './Components/LoginBtns';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+      <HomeNavbar></HomeNavbar>
+      <Body></Body>
+      <LoginBtns></LoginBtns>
+      </div>
   );
 }
 
