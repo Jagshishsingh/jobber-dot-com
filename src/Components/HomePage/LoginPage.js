a4a59e915db3b9eaf012fe7b4c2cec0e7fcd1780
Jagshish Singh <singhjagshish0001@gmail.com>
2020-10-11 10:07:35 +0530

    wrapped review components in material ui and added styling in footer

--- src/Components/HomePage/LoginPage.js
+++ src/Components/HomePage/LoginPage.js
@@ -1,113 +1,41 @@
-import React, { useState,useRef } from 'react'
+import React, { useState } from 'react'
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-import { TextField, Button, MenuItem, Menu } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import './LoginPage.css';
-import SignUp from './SignUp';
 
 // -----------URL's-----------
-const EMAIL_DATA_URL = 'https://localhost:1000' 
+const EMAIL_DATA_URL = 'https://localhost:1000'
 
 
 function LoginPage({ login, setLogin, values }) {
     const initialLoginDataState = {
-        "email":"",
-        "password":"",
-        'emailError':false,
-        'passwordError':false
+        "email": "",
+        "password": "",
+        'emailError': false,
+        'passwordError': false
     }
-    const [emailVerifed, setEmailVerifed] = useState(false);
     const [loginData, setLoginData] = useState(initialLoginDataState);
-    const [passwordVerifed, setPasswordlVerifed] = useState(false);
-    const [signUp, setSignUp] = useState(false);
-    const [forgotPassword, setforgotPassword] = useState(false)
 
-
-
-    let emailAuthentication = (e) => { // CALLED WHEN EMAIL IS ENTERED
+    let emailAuthentication = () => { // CALLED WHEN EMAIL IS ENTERED
         console.log(loginData["email"])
-        function emailFetch(){
+        function emailFetch() {
             let checkEmailExistence = false;
             // const checkEmailExistence = await fetch(`${EMAIL_DATA_URL}/:email`)  // FETCH DATA FROM SERVER
             // make this function async
             return checkEmailExistence
-            
         }
-        ;
-        if (emailFetch()){
-            e(true);  // AUTHENTICATION 
-            setLogin(false); // CLOSING CURRENT EMAIL MODAL
+        if (emailFetch()) {
             return 'FOUND';
         }
-        setLoginData({emailError:true, email:"",password:""})
-        // console.log('NOT FOUND');
-        
-        // console.log('ref value:',emailRef.current);
-        // return 'NOT FOUND';
-        
-    }
+        setLoginData({ emailError: true, email: "", password: "" })
 
-    let passwordAuthentication = (e) => {
-        e(true);  // AUTHENTICATION
-        setEmailVerifed(false) // CLOSING CURRENT PASSWORD MODAL
     }
     return (
         <div>
-            {/* ------------------EMAIL MODAL---------------------- */}
-            <Modal open={login} onClose={() => { setLogin(false) }} center
-                classNames={{
-                    overlay: 'customOverlay',
-                    modal: 'customModal',
-                }}
-            >
-                <div className="login">
-                    <h3>Login</h3>
-                    <p>{values.customerType} Email </p>
-                    <div className="">
-                        <form action="" method="post">
-                            <TextField  
-                            error = {loginData['emailError']}
-                            label="Enter Email" name="email" className="email" onChange={(e)=>setLoginData({"email":e.target.value})}/>
-                            <Button variant="contained" color="primary" style={{ 'display': 'block', 'marginTop': '1rem' }}
-                                onClick={() => emailAuthentication(setEmailVerifed)}
-                            > Click</Button>
-                        </form>
-                        <Button color="primary" onClick={() => { setLogin(false); setSignUp(true); }}>Sign Up Instead !</Button>
-                    </div>
-                </div>
+            {/* ------------------LOGIN-SIGNUP MODAL---------------------- */}
+            <Modal open={login} onClose={() => { setLogin(false) }} center>
+                <h1>EMAIL MODEL HERE : {values.customerType}</h1>
             </Modal>
-
-            {/* ------------------PASSWORD MODAL-------------------- */}
-            <Modal open={emailVerifed} center onClose={() => { setEmailVerifed(false) }}
-                classNames={{
-                    overlay: 'customOverlay',
-                    modal: 'customModal',
-                }} >
-                <div className="login">
-                    <h3>Login</h3>
-                    <p>{values.customerType} Password </p>
-                    <div className="">
-                        <form action="" method="post">
-                            <TextField type="password" label="Enter Password" />
-                            <Button variant="contained" color="primary" style={{ 'display': 'block', 'margin-top': '1rem' }}
-                                onClick={() => passwordAuthentication(setPasswordlVerifed)}
-                            > Click</Button>
-                        </form>
-                        <Button color="primary" onClick={() => { setEmailVerifed(false); setforgotPassword(true); }}>Forgot Password !</Button>
-
-                    </div>
-                </div>
-            </Modal>
-
-            {/* ------------------SIGN UP MODAL-------------------- */}
-            <Modal open={signUp} center onClose={() => { setSignUp(false) }}
-                classNames={{
-                    overlay: 'customOverlay',
-                    modal: 'customModal',
-                }} >
-                    <SignUp values={values} setSignUp={setSignUp}></SignUp>
-                </Modal>
 
         </div>
     )
