66a3598e7609e00b12e104dfad300a85b41b70f6
Jagshish Singh <singhjagshish0001@gmail.com>
2020-10-14 17:39:31 +0530

    aspirant data can be fetched from client now

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 
 const aspirant = require('./routes/aspirant');
+const test = require('./routes/test');
 const app = express();
 
 mongoose.connect(process.env.DB_URL,{
@@ -20,7 +21,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/aspirant',aspirant);
 
-
+app.use('/test',test);
 
 
 
--- server/routes/aspirant.js
+++ server/routes/aspirant.js
@@ -17,7 +17,7 @@ router.post('/addUser', function (req, res) {
     })
 });
 
-router.post('/info/:_id/:field', function (req, res) {
+router.post('/addInfo/:_id/:field', function (req, res) {
     const field = req.params.field;
     aspirantModel.findByIdAndUpdate(req.params._id, {
         $push: {
@@ -28,16 +28,14 @@ router.post('/info/:_id/:field', function (req, res) {
         return res.json({ result: result });
     })
 });
-// router.post('/info/:_id/:field', function (req, res) {
-//     const field = req.params.field;
-//     aspirantModel.findByIdAndUpdate(req.params._id, {
-//         $push: {
-//             [field]: req.body
-//         }
-//     }, function (error, result) {
-//         if (error) return res.status(500).json({ error });
-//         return res.json({ result: result });
-//     })
-// });
 
+router.post('/info/:_id',function(req,res){
+    const fields = {}
+    req.body.fields.forEach(element => { fields[element]=1 });
+    // return res.send(fields);
+    aspirantModel.findById(req.params._id,fields,function(error,data){
+        if (error) return res.json({error});
+        return res.json({data});
+    })
+})
 module.exports = router
